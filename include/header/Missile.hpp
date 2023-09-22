fa19af2da285de8d2982b19745f74f779bf4241c
simonlc76100 <simon.lellouche@edu.devinci.fr>
2023-09-22 23:12:43 +0200

    add missile class

---
+++ include/header/Missile.hpp
@@ -0,0 +1,27 @@
+#ifndef MISSILE_HPP
+#define MISSILE_HPP
+
+#include "FlyingObject.hpp"
+
+class Missile : public FlyingObject{
+
+private:
+    double speed;
+    double angle;
+
+public:
+    Missile(double x, double y, double size, double speed, double angle);
+
+    double GetSpeed() const;
+    double GetAngle() const;
+
+    void SetSpeed(double speed);
+    void SetAngle(double angle);
+
+    void Move() override;
+    bool Move(double screenWidth, double screenHeight) override;
+
+};
+
+
+#endif //MISSILE_HPP
---
+++ src/Missile.cpp
@@ -0,0 +1,41 @@
+#include "header/Missile.hpp"
+#include <cmath>
+
+Missile::Missile(double x, double y, double size, double speed, double angle)
+        : FlyingObject(x, y, size), speed(speed), angle(angle) {}
+
+// Getters
+double Missile::GetSpeed() const {
+    return speed;
+}
+
+double Missile::GetAngle() const {
+    return angle;
+}
+
+// Setters
+void Missile::SetSpeed(double speed) {
+    this->speed = speed;
+}
+
+void Missile::SetAngle(double angle) {
+    this->angle = angle;
+}
+
+void Missile::Move() {
+
+    double rad = (180.0-angle) * (M_PI/180.0);
+
+    SetX(GetX() + speed * cos(rad));
+    SetY(GetY() + speed * sin(rad));
+}
+
+bool Missile::Move(double screenWidth, double screenHeight) {
+
+    this->Move();
+
+    //if(GetX()<0 || GetX() > screenWidth && GetY()<0 || GetY() > screenHeight)
+
+    return true;
+}
+
